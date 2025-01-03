0647bd7c7fbde316759ca4c9d60d1cacc30e25ca
safrizal <rzmobiledev@gmail.com>
2025-01-03 10:43:37 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+This Project is a frontend for [backend](https://github.com/rzmobiledev/backend-auth2FA) with auth 2fa project
+
+## Getting Started
+
+First, run the development server:
+
+```bash
+npm run dev
+# or
+yarn dev
+# or
+pnpm dev
+# or
+bun dev
+```
---
+++ app/(main)/layout.tsx
@@ -0,0 +1,23 @@
+import { SidebarProvider, SidebarInset } from "@/components/ui/sidebar";
+import Asidebar from "./_components/Asidebar";
+import Header from "./_components/Header";
+import { AuthProvider } from "@/context/auth-provider";
+import React from "react";
+
+export default function MainLayout({children}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <AuthProvider>
+            <SidebarProvider>
+                <Asidebar />
+                <SidebarInset>
+                    <main className="w-full">
+                        <Header />
+                        {children}
+                    </main>
+                </SidebarInset>
+            </SidebarProvider>
+        </AuthProvider>
+    );
+}
