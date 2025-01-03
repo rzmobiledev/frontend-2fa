489588a701154e752fe7a44cbecfce0c32088eaa
safrizal <rzmobiledev@gmail.com>
2025-01-03 15:44:53 +0700

    add cross origin rules in next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -12,7 +12,7 @@ const nextConfig: NextConfig = {
         return [
             {
                 // matching all API routes
-                source: "/api/:path*",
+                source: "/api/v1/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
                     { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
